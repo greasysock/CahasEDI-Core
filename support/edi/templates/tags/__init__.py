5f3051ccd2c80776912e70ffbabf3c55c8f5ac89
chris <chris.gresock@gmail.com>
2019-01-28 12:43:56 -0500

    * Finished All Tags for templates
    TODO
    
    * Finish EDI Library
    * Create as2 connector
    * Create partnership manager
    * Create inventory manager
    * Create database connector

--- support/edi/templates/tags/__init__.py
+++ support/edi/templates/tags/__init__.py
@@ -188,7 +188,8 @@ class _REF(GENERIC_TAG):
         self._append_tag(self)
         self._property_array = [
             GenericProperty(2, 3, "Reference Identification Qualifier", StatusValues.Mandatory, 128),
-            GenericProperty(1, 30, "Reference Identification", StatusValues.Conditional, 127)
+            GenericProperty(1, 30, "Reference Identification", StatusValues.Conditional, 127),
+            GenericProperty(1, 80, "Description", StatusValues.Optional, 352)
         ]
 
 
@@ -253,7 +254,7 @@ class _ITD(GENERIC_TAG):
             EmptyProperty(),
             GenericProperty(1,3, "Terms Discount Days Due", StatusValues.Conditional, 351),
             EmptyProperty(),
-            EmptyProperty(),
+            GenericProperty(1,3, "Terms Net Days", StatusValues.Optional, 386),
             EmptyProperty(),
             EmptyProperty(),
             EmptyProperty(),
@@ -439,6 +440,12 @@ class _CTT(GENERIC_TAG):
 
         self._property_array = [
             GenericProperty(1, 6, "Number of Line Items", StatusValues.Mandatory, 354),
+            GenericProperty(1, 10, "Hash Total", StatusValues.Optional, 347),
+            GenericProperty(1, 10, "Weight", StatusValues.Conditional, 81),
+            GenericProperty(2,2, "Unit or Basis for Measurement Code", StatusValues.Conditional, 355),
+            GenericProperty(1, 8, "Volume", StatusValues.Conditional, 183),
+            GenericProperty(2,2, "Unit or Basis for Measurement Code", StatusValues.Conditional, 355),
+            GenericProperty(1,80, "Description", StatusValues.Optional, 352)
         ]
 
 
@@ -497,6 +504,14 @@ class _BEG(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'BEG', "Beginning Segment for Purchase Order")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(2,2, "Transaction Set Purpose Code", StatusValues.Mandatory, 353),
+            GenericProperty(2,2, "Purchase Order Type Code", StatusValues.Mandatory, 92),
+            GenericProperty(10,10, "Purchase Order Number", StatusValues.Mandatory, 324),
+            EmptyProperty(),
+            GenericProperty(8,8,"Date", StatusValues.Mandatory, 373)
+        ]
+
 
 BEG = _BEG()
 
@@ -504,6 +519,20 @@ class _LIN(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'LIN', "Item Identification")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(1,20,"Assigned Identification", StatusValues.Optional,350),
+            GenericProperty(2,2,"Product/Service ID Qualifier", StatusValues.Mandatory, 235),
+            GenericProperty(1,48, "Product/Service ID", StatusValues.Mandatory, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Mandatory, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Mandatory, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Mandatory, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Mandatory, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Mandatory, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Mandatory, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Mandatory, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Mandatory, 234),
+        ]
 
 
 LIN = _LIN()
@@ -513,6 +542,12 @@ class _QTY(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'QTY', "Quantity Available")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,2,"Quantity Qualifier", StatusValues.Mandatory, 673),
+            GenericProperty(1,15,"Quantity", StatusValues.Conditional, 380),
+            GenericProperty(2,2,"Unit or Basis for Measurement Code", StatusValues.Mandatory, 355)
+        ]
 
 
 QTY = _QTY()
@@ -532,6 +567,10 @@ class _CSH(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'CSH', "Sales Requirements")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,2, "Sales Requirement Code", StatusValues.Optional, 563)
+        ]
+
 
 CSH = _CSH()
 
@@ -540,6 +579,16 @@ class _PWK(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'PWK', "Paperwork")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,2, "Report Type Code", StatusValues.Mandatory, 755),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            GenericProperty(1,80, "Description", StatusValues.Optional, 352)
+        ]
 
 
 PWK = _PWK()
@@ -550,6 +599,21 @@ class _TD5(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'TD5', "Carrier Details (Routing Sequence/Transit Time)")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,2, "Routing Sequence Code", StatusValues.Optional, 133),
+            GenericProperty(1,2,"Identification Code Qualifier", StatusValues.Conditional, 66),
+            GenericProperty(2,80, "Identification Code", StatusValues.Conditional, 67),
+            GenericProperty(1,2, "Transportation Method/Type Code", StatusValues.Conditional, 91),
+            GenericProperty(1,35,"Routing",StatusValues.Conditional,387),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            GenericProperty(2,2, "Service Level Code", StatusValues.Conditional, 284)
+        ]
+
 
 TD5 = _TD5()
 
@@ -558,6 +622,10 @@ class _TD4(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'TD4', "Carrier Details (Special Handling, or Hazardous Materials, or Both)")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,3,"Special Handling Code", StatusValues.Conditional, 152)
+        ]
 
 
 TD4 = _TD4()
@@ -568,6 +636,15 @@ class _PER(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'PER', "Administrative Communications Contact")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(2,2,"Contact Function Code", StatusValues.Mandatory, 366),
+            GenericProperty(1,60, "Name", StatusValues.Optional, 93),
+            GenericProperty(2,2, "Communication Number Qualifier", StatusValues.Conditional, 365),
+            GenericProperty(1,80, "Communication Number", StatusValues.Conditional, 364),
+            GenericProperty(2, 2, "Communication Number Qualifier", StatusValues.Conditional, 365),
+            GenericProperty(1,80, "Communication Number", StatusValues.Conditional, 364)
+        ]
+
 
 PER = _PER()
 
@@ -576,6 +653,20 @@ class _PO1(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'PO1', "Baseline Item Data")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(1,20, "Assigned Information", StatusValues.Optional, 350),
+            GenericProperty(1,15,"Quantity Ordered", StatusValues.Conditional, 330),
+            GenericProperty(2,2,"Unit or Basis for Measurement Code", StatusValues.Optional, 355),
+            GenericProperty(1,17,"Unit Price", StatusValues.Conditional,212),
+            EmptyProperty(),
+            GenericProperty(2,2,"Product/Service ID Qualifier", StatusValues.Conditional,235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Conditional, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Conditional, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Conditional, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Conditional, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Conditional, 234)
+        ]
 
 
 PO1 = _PO1()
@@ -586,6 +677,12 @@ class _CTP(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'CTP', "Pricing Information")
         self._append_tag(self)
 
+        self._property_array = [
+            EmptyProperty(),
+            GenericProperty(3,3,"Price Identifier Code", StatusValues.Conditional, 236),
+            GenericProperty(1,17, "Unit Price", StatusValues.Conditional, 212)
+        ]
+
 
 CTP = _CTP()
 
@@ -594,6 +691,15 @@ class _BSN(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'BSN', "Beggining Segment for Ship Notice")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,2,"Transaction Set Purpose Code", StatusValues.Mandatory, 353),
+            GenericProperty(2,20, "Shipment Identification", StatusValues.Mandatory, 396),
+            GenericProperty(8,8,"Date", StatusValues.Mandatory, 373),
+            GenericProperty(4,8,"Time", StatusValues.Mandatory, 337),
+            GenericProperty(4,4, "Hierarchical Structure Code", StatusValues.Optional, 1005),
+            GenericProperty(2,2,"Transaction Type Code", StatusValues.Conditional, 640)
+        ]
 
 
 BSN = _BSN()
@@ -604,6 +710,12 @@ class _HL(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'HL', "Hierarchical Level – Shipment")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,12,"Hierarchical ID Number", StatusValues.Mandatory, 628),
+            GenericProperty(1,12,"Hierarchical Parent ID Number", StatusValues.Optional, 734),
+            GenericProperty(1,2,"Hierarchical Level Code", StatusValues.Mandatory, 735)
+        ]
+
 
 HL = _HL()
 
@@ -612,6 +724,17 @@ class _TD1(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'TD1', "Carrier Details (Quantity and Weight)")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(3,5,"Packaging Code", StatusValues.Optional, 103),
+            GenericProperty(1,7,"Lading Quantity", StatusValues.Conditional,80),
+            EmptyProperty(),
+            EmptyProperty(),
+            EmptyProperty(),
+            GenericProperty(1,2,"Weight Qualifier",StatusValues.Optional,187),
+            GenericProperty(1,10,"Weight", StatusValues.Conditional,81),
+            GenericProperty(2,2,"Unit or Basis for Measurement Code", StatusValues.Conditional,355)
+        ]
 
 
 TD1 = _TD1()
@@ -622,6 +745,10 @@ class _PRF(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'PRF', "Purchase Order Reference")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(10,10,"Purchase Order Number",StatusValues.Mandatory,324)
+        ]
+
 
 PRF = _PRF()
 
@@ -630,6 +757,11 @@ class _MAN(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'MAN', "Marks and Numbers")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(1,2,"Marks and Numbers Qualifier", StatusValues.Mandatory, 88),
+            GenericProperty(1,48,"Marks and Numbers", StatusValues.Mandatory, 87)
+        ]
 
 
 MAN = _MAN()
@@ -640,14 +772,30 @@ class _SN1(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'SN1', "Item Detail (Shipment)")
         self._append_tag(self)
 
+        self._property_array = [
+            EmptyProperty(),
+            GenericProperty(1,10, "Number of Units Shipped", StatusValues.Mandatory, 382),
+            GenericProperty(2,2,"Unit or Basis for Measurement Code", StatusValues.Mandatory, 355)
+        ]
+
 
 SN1 = _SN1()
 
 
 class _BCH(GENERIC_TAG):
     def __init__(self):
-        GENERIC_TAG.__init__(self, b'BCH', "Beggining Segment for Purchase Order Change")
+        GENERIC_TAG.__init__(self, b'BCH', "Beginning Segment for Purchase Order Change")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,2, "Transaction Set Purpose Code", StatusValues.Mandatory, 353),
+            GenericProperty(2,2, "Purchase Order Type Code", StatusValues.Mandatory, 92),
+            GenericProperty(1,22,"Purchase Order Number", StatusValues.Mandatory, 324),
+            EmptyProperty(),
+            EmptyProperty(),
+            GenericProperty(8,8,"Date",StatusValues.Mandatory,373),
+            GenericProperty(8,8,"Date",StatusValues.Optional, 373)
+        ]
 
 
 BCH = _BCH()
@@ -658,15 +806,37 @@ class _POC(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'POC', "Line Item Change")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,20,"Assigned Identification", StatusValues.Optional, 350),
+            GenericProperty(2,2,"Change or Response Type Code", StatusValues.Mandatory, 670),
+            EmptyProperty(),
+            EmptyProperty(),
+            GenericProperty(2,2,"Unit or Basis for Measurement Code", StatusValues.Mandatory, 355),
+            GenericProperty(1,17,"Unit Price", StatusValues.Conditional, 212),
+            EmptyProperty(),
+            GenericProperty(2,2,"Product/Service ID Qualifier", StatusValues.Conditional, 235),
+            GenericProperty(1,48,"Product/Service ID", StatusValues.Conditional, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Conditional, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Conditional, 234),
+            GenericProperty(2, 2, "Product/Service ID Qualifier", StatusValues.Conditional, 235),
+            GenericProperty(1, 48, "Product/Service ID", StatusValues.Conditional, 234),
+        ]
+
 
 POC = _POC()
 
 
 class _BAK(GENERIC_TAG):
     def __init__(self):
-        GENERIC_TAG.__init__(self, b'BAK', "Beggining Segment for Purchase Order Aknowledgment")
+        GENERIC_TAG.__init__(self, b'BAK', "Beginning Segment for Purchase Order Acknowledgment")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(2,2,"Transaction Set Purpose Code", StatusValues.Mandatory, 353),
+            GenericProperty(2,2, "Acknowledgment Type", StatusValues.Mandatory, 587),
+            GenericProperty(10,10, "Purchase Order Number", StatusValues.Mandatory, 324),
+            GenericProperty(8,8, "Date", StatusValues.Mandatory,373)
+        ]
 
 BAK = _BAK()
 
@@ -676,14 +846,39 @@ class _CUR(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'CUR', "Currency")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(2,3, "Entity Identifier Code", StatusValues.Mandatory, 98),
+            GenericProperty(3,3, "Currency Code", StatusValues.Mandatory, 100)
+        ]
+
 
 CUR = _CUR()
+
+class _ACK(GENERIC_TAG):
+    def __init__(self):
+        GENERIC_TAG.__init__(self, b'ACK', "Line Item Acknowledgment")
+        self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2, 2, "Line Item Status Code", StatusValues.Mandatory, 668),
+            GenericProperty(1, 15, "Quantity", StatusValues.Mandatory, 380),
+            GenericProperty(2,2, "Unit or Basis for Measurement", StatusValues.Mandatory, 355)
+        ]
+
+
+ACK = _ACK()
+
 
 
 class _AK1(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'AK1', "Functional Group Response Header")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,2,"Functional Identifier Code", StatusValues.Mandatory, 479),
+            GenericProperty(1,9, "Group Control Number", StatusValues.Mandatory, 28)
+        ]
 
 
 AK1 = _AK1()
@@ -694,6 +889,12 @@ class _AK2(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'AK2', "Transaction Set Response Header")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(3,3,"Transaction Set Identifier Code", StatusValues.Mandatory, 143),
+            GenericProperty(4,9, "Transaction Set Control Number", StatusValues.Mandatory, 329)
+        ]
+
+
 
 AK2 = _AK2()
 
@@ -702,6 +903,13 @@ class _AK3(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'AK3', "Data Segment Note")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(2,3,"Segment ID Code", StatusValues.Mandatory, 721),
+            GenericProperty(1,6,"Segment Position in Transaction Set", StatusValues.Mandatory, 719),
+            GenericProperty(1,6,"Loop Identifier Code", StatusValues.Optional, 447),
+            GenericProperty(1,3, "Segment Syntax Error Code", StatusValues.Optional, 720)
+        ]
 
 
 AK3 = _AK3()
@@ -712,6 +920,13 @@ class _AK4(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'AK4', "Data Element Note")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,2,"Position in Segment", StatusValues.Mandatory,722),
+            GenericProperty(1,4,"Data Element Reference Number", StatusValues.Optional, 725),
+            GenericProperty(1,3,"Data Element Syntax Error Code", StatusValues.Mandatory, 723),
+            GenericProperty(1,99, "Copy of Bad Data Element", StatusValues.Optional, 724)
+        ]
+
 
 AK4 = _AK4()
 
@@ -721,6 +936,16 @@ class _AK5(GENERIC_TAG):
         GENERIC_TAG.__init__(self, b'AK5', "Transaction Set Response Trailer")
         self._append_tag(self)
 
+        self._property_array = [
+            GenericProperty(1,1, "Transaction Set Acknowledgment Code", StatusValues.Mandatory, 717),
+            GenericProperty(1,3, "Transaction Set Syntax Error Code", StatusValues.Optional, 718),
+            GenericProperty(1, 3, "Transaction Set Syntax Error Code", StatusValues.Optional, 718),
+            GenericProperty(1, 3, "Transaction Set Syntax Error Code", StatusValues.Optional, 718),
+            GenericProperty(1, 3, "Transaction Set Syntax Error Code", StatusValues.Optional, 718),
+            GenericProperty(1, 3, "Transaction Set Syntax Error Code", StatusValues.Optional, 718),
+
+        ]
+
 
 AK5 = _AK5()
 
@@ -729,6 +954,19 @@ class _AK9(GENERIC_TAG):
     def __init__(self):
         GENERIC_TAG.__init__(self, b'AK9', "Functional Group Response Trailer")
         self._append_tag(self)
+
+        self._property_array = [
+            GenericProperty(1,1, "Functional Group Acknowledge Code", StatusValues.Mandatory, 715),
+            GenericProperty(1,6, "Number of Transaction Sets Included", StatusValues.Mandatory,97),
+            GenericProperty(1,6,"Number of Received Transaction Sets",StatusValues.Mandatory, 123),
+            GenericProperty(1,6, "Number of Accepted Transaction Sets", StatusValues.Mandatory, 2),
+            GenericProperty(1,3, "Functional Group Syntax Error Code", StatusValues.Optional, 716),
+            GenericProperty(1, 3, "Functional Group Syntax Error Code", StatusValues.Optional, 716),
+            GenericProperty(1, 3, "Functional Group Syntax Error Code", StatusValues.Optional, 716),
+            GenericProperty(1, 3, "Functional Group Syntax Error Code", StatusValues.Optional, 716),
+            GenericProperty(1, 3, "Functional Group Syntax Error Code", StatusValues.Optional, 716),
+
+        ]
 
 
 AK9 = _AK9()
